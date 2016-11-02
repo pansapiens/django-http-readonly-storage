a3991e8c557112e03a84cf238492abd0ffc94f9c
Andrew Perry <ajperry@pansapiens.com>
2016-11-02 16:59:31 +1100

    Seeking, add closed property, cache things found in initial response headers (eg size on open).

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # django-http-readonly-storage
 A storage backend for Django that reads files from HTTP(S) URLs
+
+Warning: This isn't complete or well tested (no tests yet !).
+It was primarily written as a read-only storage backend for the
+MyTardis server, where it appears to work for my purposes. YMMV.
--- django_http_storage/backends/http.py
+++ django_http_storage/backends/http.py
@@ -7,7 +7,7 @@ from pathlib2 import Path
 import cgi
 from urlparse import urlparse, urljoin
 import io
-from io import BytesIO
+from io import BytesIO, IOBase
 from django.core.files.storage import Storage as StorageBase
 from django.core.files.base import File
 import requests
@@ -17,6 +17,7 @@ class HTTPReadOnlyStorage(StorageBase):
     """
     A read-only HTTP endpoint storage backend.
     """
+    headers = {}
 
     def __init__(self, **kwargs):
         """
@@ -52,11 +53,15 @@ class HTTPReadOnlyStorage(StorageBase):
     # def get_requests_instance(self, **kwargs):
     #     return requests.Session()
 
-    def http(self, method, name, *args, **kwargs):
+    def http(self, method, name, extra_headers=None, *args, **kwargs):
         url = self.get_download_url(name)
         method = method.lower()
         # response = getattr(self.session, method)(url, *args, **kwargs)
-        response = requests.request(method, url, *args, **kwargs)
+        headers = self.headers.copy()
+        if extra_headers is not None:
+            headers.update(extra_headers)
+        response = requests.request(method, url, headers=headers,
+                                    *args, **kwargs)
         response.raise_for_status()
         return response
 
@@ -90,16 +95,17 @@ class HTTPReadOnlyStorage(StorageBase):
         else:
             return True
 
-    def size(self, url):
+    def size(self, name):
         """
         Tries to determine the file size for a given download URL via the
         Content-Length header.
 
-        :param url: The URL
+        :param name: The filename (or URL)
         :type url: str
         :return: The file/download size in bytes
         :rtype: int
         """
+        url = self.get_download_url(name)
         try:
             return int(self.http('HEAD', url).headers['Content-Length'])
         except (KeyError, ValueError, requests.exceptions.HTTPError):
@@ -160,7 +166,14 @@ class HTTPReadOnlyStorageFile(File):
 
     @property
     def file(self):
-        return self
+        self._response.raw.size = self.size
+        return self._response.raw
+
+    @property
+    def closed(self):
+        if self._response is None:
+            return True
+        return self._response.raw.closed
 
     @property
     def size(self):
@@ -172,32 +185,79 @@ class HTTPReadOnlyStorageFile(File):
                 self._size = self._storage.size(self.name)
         return self._size
 
+    def seekable(self):
+        return self._seekable
+
+    def seek(self, offset, whence=io.SEEK_SET):
+        if not self.seekable():
+            raise OSError("Stream not seekable. "
+                          "Server doesn't support Range requests.")
+
+        self.close()
+        self._open_request(extra_headers={'Range': 'bytes=%d-' % offset})
+
+        # TODO: Support proper whence, tracking read offset as self._offset
+        # TODO: We should check the actual Content-Range header in
+        #       self._response to return the offset value here
+
+        return min(offset, self.size)
+
     def readlines(self):
         if self.stream:
             return self._response.iter_lines()
         else:
             return str(self._file.read()).split()
 
-    def _open(self):
-        self.close()
+    def _open(self, extra_headers=None):
+        if self.closed:
+            raise ValueError("Request is closed.")
+
+        self._response = self._open_request(extra_headers=extra_headers)
+
+        return self
+
+    def _open_request(self, extra_headers=None):
+
         # We make HTTP requests via a method on the Storage object,
         # since this potetially allows the Storage object to inject
         # authentication headers etc
+        if extra_headers is None:
+            extra_headers = {}
+
+        # Always specifing Range allows use to determine if the server
+        # accepts Range requests by the response headers, allowing us
+        # to set 'seekable immediately
+        if not 'Range' in extra_headers:
+            extra_headers['Range'] = 'bytes=0-'
+
         self._response = self._storage.http('GET', self.name,
-                                            stream=self.stream)
+                                            stream=self.stream,
+                                            extra_headers=extra_headers)
+
+        if 'Content-Length' in self._response.headers:
+            self._size = int(self._response.headers['Content-Length'])
+
+        self._seekable = ((self._response.status_code == 206) or
+                          ('Content-Range' in self._response.headers))
+
         if self.stream:
+            self._response.raw.decode_content = True
             self._content = self._response.iter_content(
                 chunk_size=self._chunk_size)
         else:
-            self._file = BytesIO(self._response.content)
+            data = self._response.content
+            self._size = len(data)
+            self._file = BytesIO(data)
 
-        return self
+        return self._response
 
     def read(self, num_bytes=2**16):
         if self.stream:
             data = self._content.next()
+            # data = self._response.raw.read(num_bytes)
         else:
             data = self._file.read(num_bytes)
+
         return data
 
     # def chunks(self, chunk_size=File.DEFAULT_CHUNK_SIZE):
@@ -208,6 +268,11 @@ class HTTPReadOnlyStorageFile(File):
 
     def write(self, content):
         raise NotImplementedError()
+
+    @property
+    def closed(self):
+        if self._response:
+            return self._response.raw.closed
 
     def close(self):
         if self._response:
